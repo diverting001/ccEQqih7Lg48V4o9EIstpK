532764102f948ac2646ac6ae98d505339534ff7b
diverting001 <diverting001@outlook.de>
2025-05-08 14:58:50 +0800

    Delete m4 directory

--- m4/vendor/NG_THRIFT_CLIENT/ExpressClient/inc/thrift.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: liyunlong
- * Date: 2015/7/8
- * Time: 15:05
- */
-include_once __DIR__ . '/../../lib/php/lib/Thrift/ClassLoader/ThriftClassLoader.php';
