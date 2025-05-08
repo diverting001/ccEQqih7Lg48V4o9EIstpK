86e46b64c8c2e8ff1f6d63ec7d6e52e8529e9aac
diverting001 <diverting001@outlook.de>
2025-05-08 14:58:21 +0800

    Delete cofco directory

--- cofco/routes/api/v4.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//-----------------------------计算服务-start------------------------//
-$api->post('Calculate/Get', 'App\Api\V4\Controllers\CalculateController@priceCalculate');
-$api->post('Calculate/GetLog', 'App\Api\V4\Controllers\CalculateController@getLog');
-//-----------------------------计算服务-end--------------------------//
